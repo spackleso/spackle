78809ec583b99aaf985837f9e4499c2e8a7d34be
Hunter Clarke <hunter@hunterclarke.me>
2022-10-16 13:51:01 -0600

    meh revert

--- src/pages/api/stripe/connected_webhooks.ts
+++ src/pages/api/stripe/connected_webhooks.ts
@@ -6,7 +6,7 @@ export default async function handler(
   res: NextApiResponse<any>,
 ) {
   const sig = req.headers['stripe-signature'] as string
-  const payload = req.body
+  const payload = JSON.stringify(req.body)
 
   let event
   try {
