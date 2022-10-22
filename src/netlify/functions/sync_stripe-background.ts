783ba1eaf533ab06a393ab26a9b125fc84436360
Hunter Clarke <hunter@hunterclarke.me>
2022-10-21 21:24:08 -0600

    adds types on background handler

--- src/netlify/functions/sync_stripe-background.ts
+++ src/netlify/functions/sync_stripe-background.ts
@@ -9,16 +9,21 @@ Sentry.AWSLambda.init({
   tracesSampleRate: 1.0,
 })
 
-export const handler = Sentry.AWSLambda.wrapHandler(async (event, context) => {
-  if (!event.queryStringParameters || !event.queryStringParameters.account_id) {
-    console.log('Invalid request', event)
-    return
-  }
+export const handler: BackgroundHandler = Sentry.AWSLambda.wrapHandler(
+  async (event, context) => {
+    if (
+      !event.queryStringParameters ||
+      !event.queryStringParameters.account_id
+    ) {
+      console.log('Invalid request', event)
+      return
+    }
 
-  const { account_id } = event.queryStringParameters
-  if (account_id) {
-    await syncAllAccountData(account_id)
-  } else {
-    console.log(`Invalid account: ${account_id}`, event.body)
-  }
-})
+    const { account_id } = event.queryStringParameters
+    if (account_id) {
+      await syncAllAccountData(account_id)
+    } else {
+      console.log(`Invalid account: ${account_id}`, event.body)
+    }
+  },
+) as BackgroundHandler
