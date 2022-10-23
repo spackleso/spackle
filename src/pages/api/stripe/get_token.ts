a8342e1a63f324305e9d771cd23af718918696c3
Hunter Clarke <hunter@hunterclarke.me>
2022-10-23 00:05:57 -0600

    adds tokens table for tracking api tokens

---
+++ src/pages/api/stripe/get_token.ts
@@ -0,0 +1,84 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { checkCors } from '../../../cors'
+import { withLogging } from '../../../logger'
+import { verifySignature } from '../../../stripe/signature'
+import { syncStripeAccount } from '../../../stripe/sync'
+import { supabase } from '../../../supabase'
+import * as Sentry from '@sentry/nextjs'
+import jwt from 'jsonwebtoken'
+
+const SIGNING_KEY = process.env.SUPABASE_JWT_SECRET
+
+const fetchToken = async (account_id: string) => {
+  const response = await supabase
+    .from('tokens')
+    .select('token')
+    .eq('stripe_account_id', account_id)
+
+  if (response.error) {
+    throw new Error(response.error.message)
+  }
+
+  return response
+}
+
+const createToken = async (account_id: string) => {
+  console.log('check')
+  if (!SIGNING_KEY) {
+    throw new Error('Signing key not set')
+  }
+
+  const response = await supabase
+    .from('tokens')
+    .insert({
+      stripe_account_id: account_id,
+      token: jwt.sign(
+        {
+          sub: account_id,
+          iat: Math.floor(Date.now() / 1000),
+        },
+        SIGNING_KEY,
+      ),
+    })
+    .select()
+
+  console.log(response)
+
+  if (response.error) {
+    throw new Error(response.error.message)
+  }
+
+  return response
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await checkCors(req, res)
+
+  const { success } = verifySignature(req)
+  if (!success) {
+    return res.status(400).send('')
+  }
+
+  // TODO: handle all errors
+  const { account_id } = req.body
+
+  await syncStripeAccount(account_id)
+
+  let token
+  try {
+    const { data: tokens } = await fetchToken(account_id)
+    if (tokens.length) {
+      token = tokens[0].token
+    } else {
+      const { data: tokens } = await createToken(account_id)
+      token = tokens[0].token
+    }
+  } catch (error) {
+    Sentry.captureException(error)
+    return res.status(400).json({})
+  }
+
+  res.status(200).json({ token })
+}
+
+export default withLogging(handler)
