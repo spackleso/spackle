e55a100b1ce7a1f394c8eab6c8313e305a04a4bc
Hunter Clarke <hunter@hunterclarke.me>
2022-10-28 15:48:33 -0600

    fixes home page typo

--- src/components/Marketing/Hero.tsx
+++ src/components/Marketing/Hero.tsx
@@ -29,7 +29,9 @@ export function Hero() {
     <Container className="pt-20 pb-16 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
         Custom SaaS billing{' '}
-        <span className="relative whitespace-nowrap text-blue-600">withou</span>{' '}
+        <span className="relative whitespace-nowrap text-blue-600">
+          without
+        </span>{' '}
         the code complexity
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
