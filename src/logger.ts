f1f74461b701c66b13c2306d27a9c9049c8ed185
Hunter Clarke <hunter@hunterclarke.me>
2022-11-02 09:22:02 -0600

    trying to fix logtail

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,9 @@
 import { Logtail } from '@logtail/node'
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
-export const logger =
-  process.env.NODE_ENV === 'production'
-    ? new Logtail(process.env.LOGTAIL_API_KEY || '')
-    : console
+export const logger = process.env.LOGTAIL_API_KEY
+  ? new Logtail(process.env.LOGTAIL_API_KEY || '')
+  : console
 
 export const withLogging = (handler: NextApiHandler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
