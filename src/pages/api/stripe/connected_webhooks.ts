32c41c21872dc22c4fea792b02fcd486e65cb51e
Hunter Clarke <hunter@hunterclarke.me>
2022-11-11 21:04:32 -0700

    fixes connected webhooks

--- src/pages/api/stripe/connected_webhooks.ts
+++ src/pages/api/stripe/connected_webhooks.ts
@@ -43,6 +43,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       throw err
     }
 
+    syncStripeAccount(event.account!)
+
     if (event.type === 'account.updated') {
       await syncStripeAccount((event.data.object as Stripe.Account).id)
     } else if (event.type === 'account.application.authorized') {
