b285bc6e274ddeda5175987166197497be125c55
Hunter Clarke <hunter@hunterclarke.me>
2022-12-17 21:23:27 -0700

    debugging cognito

--- src/cognito.ts
+++ src/cognito.ts
@@ -3,23 +3,35 @@ import {
   GetOpenIdTokenForDeveloperIdentityCommand,
 } from '@aws-sdk/client-cognito-identity'
 
-const { SPACKLE_AWS_ACCESS_KEY_ID, SPACKLE_AWS_SECRET_ACCESS_KEY } = process.env
+const {
+  SPACKLE_AWS_ACCESS_KEY_ID,
+  SPACKLE_AWS_SECRET_ACCESS_KEY,
+  AWS_COGNITO_IDENTITY_POOL_ID,
+  AWS_COGNITO_IDENTITY_PROVIDER,
+} = process.env
 
 export const getIdentityToken = async (accountId: string) => {
-  if (!SPACKLE_AWS_ACCESS_KEY_ID || !SPACKLE_AWS_SECRET_ACCESS_KEY) {
+  if (
+    !SPACKLE_AWS_ACCESS_KEY_ID ||
+    !SPACKLE_AWS_SECRET_ACCESS_KEY ||
+    !AWS_COGNITO_IDENTITY_POOL_ID ||
+    !AWS_COGNITO_IDENTITY_PROVIDER
+  ) {
     throw new Error('Missing AWS credentials')
   }
 
   const client = new CognitoIdentityClient({
+    region: 'us-west-2',
     credentials: {
       accessKeyId: SPACKLE_AWS_ACCESS_KEY_ID,
       secretAccessKey: SPACKLE_AWS_SECRET_ACCESS_KEY,
     },
   })
+
   const command = new GetOpenIdTokenForDeveloperIdentityCommand({
-    IdentityPoolId: process.env.AWS_COGNITO_IDENTITY_POOL_ID || '',
+    IdentityPoolId: AWS_COGNITO_IDENTITY_POOL_ID,
     Logins: {
-      [process.env.AWS_COGNITO_IDENTITY_PROVIDER || '']: accountId,
+      [AWS_COGNITO_IDENTITY_PROVIDER]: accountId,
     },
   })
 
