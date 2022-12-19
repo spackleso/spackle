82253b00f0e159ebf8cbb72f8b4bd3d7e82e3c62
Hunter Clarke <hunter@hunterclarke.me>
2022-12-19 11:30:14 -0700

    adds customer state test

--- src/state.test.ts
+++ src/state.test.ts
@@ -2,6 +2,8 @@ import crypto from 'crypto'
 import { execSync } from 'child_process'
 import {
   getAccountFeaturesState,
+  getCustomerFeaturesState,
+  getCustomerState,
   getPriceFeaturesState,
   getProductFeaturesState,
 } from './state'
@@ -163,6 +165,87 @@ test('Get price state should return overridden account features', async () => {
       id: feature.id,
       key: 'feature',
       name: 'Feature',
+      type: 0,
+      value_flag: true,
+      value_limit: null,
+    },
+  ])
+})
+
+test('Get customer state should return overridden subscription features', async () => {
+  const { data: accountData } = (await supabase
+    .from('stripe_accounts')
+    .insert({
+      stripe_id: stripeId('acct'),
+    })
+    .select()) as any
+  const account = accountData[0]
+
+  const { data: customerData } = (await supabase
+    .from('stripe_customers')
+    .insert({
+      stripe_id: stripeId('acct'),
+      stripe_account_id: account.stripe_id,
+    })
+    .select()) as any
+  const customer = customerData[0]
+
+  const { data: trueFeatureData } = await supabase
+    .from('features')
+    .insert({
+      name: 'Default True',
+      key: 'default_true',
+      type: 0,
+      value_flag: true,
+      stripe_account_id: account.stripe_id,
+    })
+    .select()
+  const trueFeature = trueFeatureData![0]
+
+  const { data: falseFeatureData } = await supabase
+    .from('features')
+    .insert({
+      name: 'Default False',
+      key: 'default_false',
+      type: 0,
+      value_flag: false,
+      stripe_account_id: account.stripe_id,
+    })
+    .select()
+  const falseFeature = falseFeatureData![0]
+
+  await supabase.from('customer_features').insert({
+    value_flag: true,
+    stripe_account_id: account.stripe_id,
+    stripe_customer_id: customer.stripe_id,
+    feature_id: falseFeature.id,
+  })
+
+  await supabase.from('customer_features').insert({
+    value_flag: false,
+    stripe_account_id: account.stripe_id,
+    stripe_customer_id: customer.stripe_id,
+    feature_id: trueFeature.id,
+  })
+
+  const state = await getCustomerFeaturesState(
+    account.stripe_id,
+    customer.stripe_id,
+  )
+
+  expect(state).toStrictEqual([
+    {
+      id: trueFeature.id,
+      key: 'default_true',
+      name: 'Default True',
+      type: 0,
+      value_flag: false,
+      value_limit: null,
+    },
+    {
+      id: falseFeature.id,
+      key: 'default_false',
+      name: 'Default False',
       type: 0,
       value_flag: true,
       value_limit: null,
