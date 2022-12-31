ee6a510d00f02952265d57b53437a0998fe014b4
Hunter Clarke <hunter@hunterclarke.me>
2022-12-31 16:56:36 -0700

    proxies api calls to the new api service

--- apps/stripe/src/hooks/useApi.ts
+++ apps/stripe/src/hooks/useApi.ts
@@ -2,11 +2,11 @@ import { ExtensionContextValue } from '@stripe/ui-extension-sdk/context'
 import fetchStripeSignature from '@stripe/ui-extension-sdk/signature'
 import { useContext, createContext } from 'react'
 
-const isDev = true
+const isDev = false
 
 let HOST = 'https://www.spackle.so'
 if (isDev) {
-  HOST = 'http://localhost:3001'
+  HOST = 'http://localhost:3000'
 }
 
 interface Api {
