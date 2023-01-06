37ae0c1d31afa7ce57c02615a4c4dc7b359b8d59
Hunter Clarke <hunter@hunterclarke.me>
2023-01-05 20:07:11 -0700

    debugging test errors

--- apps/api/src/supabase.ts
+++ apps/api/src/supabase.ts
@@ -4,7 +4,10 @@ import { Database } from './schema.gen'
 const supabaseUrl = process.env.SUPABASE_URL || ''
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || ''
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(
+  'http://localhost:54321',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs',
+)
 
 export class SupabaseError extends Error {
   error: PostgrestError
