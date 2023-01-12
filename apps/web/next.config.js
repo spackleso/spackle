357693c37f3f5b5f4709b57332a03232a5fd3121
Hunter Clarke <hunter@hunterclarke.me>
2023-01-12 05:37:48 -0700

    adds deprecated redirects

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.spackle.so/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/:path*',
+        destination: 'https://docs.spackle.so/:path*',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
