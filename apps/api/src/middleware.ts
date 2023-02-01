dfac870b040bdf088e6e8ef42fb8c28c7bf2cef1
Hunter Clarke <hunter@hunterclarke.me>
2023-02-01 08:38:12 -0700

    testing log forwarding

--- apps/api/src/middleware.ts
+++ apps/api/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  console.log('Testing middleware')
   const response = NextResponse.next()
   return response
 }
