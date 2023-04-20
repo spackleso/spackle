e85cfb9530189a57675de0eff3cc4dc239fd7753
Hunter Clarke <hunter@hunterclarke.me>
2023-04-20 05:57:14 -0600

    removes analytics & upgrades turbo

--- apps/web/src/app/template.tsx
+++ apps/web/src/app/template.tsx
@@ -2,13 +2,9 @@
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/query'
-import { Analytics } from '@vercel/analytics/react'
 
 export default function Template({ children }: any) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <Analytics />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
