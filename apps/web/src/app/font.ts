efceae2031adc381365cba6f1a3d51f27e6b6038
Hunter Clarke <hunter@hunterclarke.me>
2023-04-20 20:09:46 -0600

    debugging local font config

--- apps/web/src/app/font.ts
+++ apps/web/src/app/font.ts
@@ -3,17 +3,14 @@ import { Comfortaa, Inter, Lexend } from 'next/font/google'
 export const inter = Inter({
   variable: '--font-inter',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  display: 'swap',
 })
 
 export const lexend = Lexend({
   variable: '--font-lexend',
   weight: ['400', '500'],
-  display: 'swap',
 })
 
 export const comfortaa = Comfortaa({
   variable: '--font-comfortaa',
   weight: ['300', '400', '500', '600', '700'],
-  display: 'swap',
 })
