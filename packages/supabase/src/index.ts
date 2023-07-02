382cadb440e3bf5fa177111dde61e6736012cb80
Hunter Clarke <hunter@hunterclarke.me>
2023-07-02 15:29:36 -0400

    uses service key for api access to db

--- packages/supabase/src/index.ts
+++ packages/supabase/src/index.ts
@@ -2,8 +2,7 @@ import { createClient, PostgrestError } from '@supabase/supabase-js'
 import { Database } from './schema.gen'
 
 const supabaseUrl = process.env.SUPABASE_URL || ''
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || ''
-
+const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
 export class SupabaseError extends Error {
---
+++ supabase/migrations/20230702192904_rls.sql
@@ -0,0 +1,29 @@
+alter table "public"."customer_features" enable row level security;
+
+alter table "public"."features" enable row level security;
+
+alter table "public"."invites" enable row level security;
+
+alter table "public"."price_features" enable row level security;
+
+alter table "public"."product_features" enable row level security;
+
+alter table "public"."stripe_accounts" enable row level security;
+
+alter table "public"."stripe_customers" enable row level security;
+
+alter table "public"."stripe_prices" enable row level security;
+
+alter table "public"."stripe_products" enable row level security;
+
+alter table "public"."stripe_subscription_items" enable row level security;
+
+alter table "public"."stripe_subscriptions" enable row level security;
+
+alter table "public"."stripe_users" enable row level security;
+
+alter table "public"."tokens" enable row level security;
+
+alter table "public"."wait_list_entries" enable row level security;
+
+
