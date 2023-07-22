91723aa36b010e180f7087e888906a6aca9d2710
Hunter Clarke <hunter@hunterclarke.me>
2023-07-22 11:22:29 -0600

    edge server

---
+++ apps/edge/src/index.ts
@@ -0,0 +1,65 @@
+import express from 'express'
+import { IncomingHttpHeaders } from 'http'
+import jwt from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb'
+
+dotenv.config({ path: __dirname + '/.env' })
+
+const app = express()
+
+const { SUPABASE_JWT_SECRET, DYNAMODB_TABLE_NAME } = process.env
+const port = process.env.PORT || 3003
+const identityId = 'us-west-2:975730cb-224e-4896-bc1c-987bf15c8986'
+const client = new DynamoDBClient({ region: 'us-west-2' })
+
+app.get('/', (req, res) => {
+  res.send('')
+})
+
+app.get('/customers/:id/state', async (req, res) => {
+  try {
+    requestToken(req.headers)
+  } catch (error) {
+    res.status(401).send('')
+    return
+  }
+
+  const item = await client.send(
+    new GetItemCommand({
+      TableName: DYNAMODB_TABLE_NAME,
+      Key: {
+        AccountId: {
+          S: identityId,
+        },
+        CustomerId: {
+          S: `${req.params.id}:1`,
+        },
+      },
+    }),
+  )
+
+  res.json(item.Item?.State.S)
+})
+
+app.listen(port, () => {
+  console.log(`The application is listening on port ${port}!`)
+})
+
+const requestToken = (headers: IncomingHttpHeaders) => {
+  if (!SUPABASE_JWT_SECRET) {
+    throw new Error('Signing key not set')
+  }
+
+  const authorization = headers['authorization'] || 'Bearer '
+  const token = authorization.split(' ')[1]
+  const payload = jwt.verify(token, SUPABASE_JWT_SECRET)
+
+  if (!payload.sub) {
+    throw new Error('Invalid jwt')
+  }
+
+  return {
+    sub: payload.sub as string,
+  }
+}
