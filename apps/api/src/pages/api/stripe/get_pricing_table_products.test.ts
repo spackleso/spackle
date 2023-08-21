1c4db3ab82353b4ae5b27681e728d0374a633533
Hunter Clarke <hunter@hunterclarke.me>
2023-08-21 08:31:36 -0600

    wip

--- apps/api/src/pages/api/stripe/get_pricing_table_products.test.ts
+++ apps/api/src/pages/api/stripe/get_pricing_table_products.test.ts
@@ -1,11 +1,13 @@
 /**
  * @jest-environment node
  */
-import handler from '@/pages/api/stripe/get_pricing_tables'
+import handler from '@/pages/api/stripe/get_pricing_table_products'
 import {
   createAccount,
+  createFlagFeature,
   createPricingTable,
   createPricingTableProduct,
+  createStripePrice,
   createStripeProduct,
   stripeTestHandler,
   testHandler,
@@ -28,6 +30,12 @@ describe('POST', () => {
 
   test("Returns a pricing table's products", async () => {
     const account = await createAccount()
+    const feature = await createFlagFeature(
+      account.stripeId,
+      'Feature 1',
+      'feature_1',
+      true,
+    )
     const pricingTable = await createPricingTable(
       account.stripeId,
       'Default',
@@ -35,23 +43,47 @@ describe('POST', () => {
       true,
       true,
     )
-    const product1 = await createStripeProduct(account.stripeId)
-    const product2 = await createStripeProduct(account.stripeId)
-    await createPricingTableProduct(
+    const product1 = await createStripeProduct(account.stripeId, {
+      name: 'Product 1',
+    })
+    const p1Monthly = await createStripePrice(
+      account.stripeId,
+      product1.stripeId,
+    )
+    const p1Annual = await createStripePrice(
+      account.stripeId,
+      product1.stripeId,
+    )
+    const ptp1 = await createPricingTableProduct(
       account.stripeId,
       pricingTable.id,
       product1.stripeId,
+      p1Monthly.stripeId,
+      p1Annual.stripeId,
     )
-    await createPricingTableProduct(
+    const product2 = await createStripeProduct(account.stripeId, {
+      name: 'Product 2',
+    })
+    const p2Monthly = await createStripePrice(
+      account.stripeId,
+      product2.stripeId,
+    )
+    const p2Annual = await createStripePrice(
+      account.stripeId,
+      product2.stripeId,
+    )
+    const ptp2 = await createPricingTableProduct(
       account.stripeId,
       pricingTable.id,
       product2.stripeId,
+      p2Monthly.stripeId,
+      p2Annual.stripeId,
     )
     const res = await stripeTestHandler(handler, {
       method: 'POST',
       body: {
         account_id: account.stripeId,
-        mode: 'live',
+        pricing_table_id: pricingTable.id,
       },
     })
 
@@ -59,18 +91,36 @@ describe('POST', () => {
     const data = res._getJSONData()
     expect(data).toStrictEqual([
       {
-        id: product1.id,
-        name: null,
-        features: [],
-        monthly_stripe_price: null,
-        annual_stripe_price: null,
+        id: ptp1.id,
+        name: 'Product 1',
+        features: [
+          {
+            id: feature.id,
+            name: 'Feature 1',
+            key: 'feature_1',
+            type: 0,
+            value_flag: true,
+            value_limit: null,
+          },
+        ],
+        monthly_stripe_price: p1Monthly.stripeJson,
+        annual_stripe_price: p1Annual.stripeJson,
       },
       {
-        id: product1.id,
-        name: null,
-        features: [],
-        monthly_stripe_price: null,
-        annual_stripe_price: null,
+        id: ptp2.id,
+        name: 'Product 2',
+        features: [
+          {
+            id: feature.id,
+            name: 'Feature 1',
+            key: 'feature_1',
+            type: 0,
+            value_flag: true,
+            value_limit: null,
+          },
+        ],
+        monthly_stripe_price: p2Monthly.stripeJson,
+        annual_stripe_price: p2Annual.stripeJson,
       },
     ])
   })
--- apps/api/src/pages/api/stripe/get_pricing_table_products.ts
+++ apps/api/src/pages/api/stripe/get_pricing_table_products.ts
@@ -1,7 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { verifySignature } from '@/stripe/signature'
-import db, { pricingTableProducts } from 'spackle-db'
+import db, {
+  pricingTableProducts,
+  stripePrices,
+  stripeProducts,
+} from 'spackle-db'
 import { and, eq } from 'drizzle-orm'
+import { getProductFeaturesState } from '@/state'
+import Stripe from 'stripe'
+import { alias } from 'drizzle-orm/pg-core'
+
+enum FeatureType {
+  Flag = 0,
+  Limit = 1,
+}
+
+type Feature = {
+  id: number
+  key: string
+  name: string
+  type: FeatureType
+  value_flag: boolean | null
+  value_limit: number | null
+}
+
+type PricingTableProductData = {
+  id: number
+  name: string
+  features: Feature[]
+  monthly_stripe_price?: Stripe.Price
+  annual_stripe_price?: Stripe.Price
+}
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { success } = verifySignature(req)
@@ -12,11 +41,32 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   const { account_id, pricing_table_id } = req.body
+  const monthlyStripePrices = alias(stripePrices, 'monthlyStripePrices')
+  const annualStripePrices = alias(stripePrices, 'annualStripePrices')
   const pricingTableResult = await db
     .select({
       id: pricingTableProducts.id,
+      stripeProductId: pricingTableProducts.stripeProductId,
+      stripeProductStripeJson: stripeProducts.stripeJson,
+      monthlyStripePrice: monthlyStripePrices.stripeJson,
+      annualStripePrice: annualStripePrices.stripeJson,
     })
     .from(pricingTableProducts)
+    .leftJoin(
+      stripeProducts,
+      eq(pricingTableProducts.stripeProductId, stripeProducts.stripeId),
+    )
+    .leftJoin(
+      monthlyStripePrices,
+      eq(
+        pricingTableProducts.monthlyStripePriceId,
+        monthlyStripePrices.stripeId,
+      ),
+    )
+    .leftJoin(
+      annualStripePrices,
+      eq(pricingTableProducts.annualStripePriceId, annualStripePrices.stripeId),
+    )
     .where(
       and(
         eq(pricingTableProducts.stripeAccountId, account_id),
@@ -24,7 +74,26 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       ),
     )
 
-  return res.status(200).json(pricingTableResult)
+  const data: PricingTableProductData[] = []
+
+  // TODO: Promise.all the state call
+  for (const pricingTableProduct of pricingTableResult) {
+    data.push({
+      id: pricingTableProduct.id,
+      name:
+        (pricingTableProduct.stripeProductStripeJson as Stripe.Product)?.name ||
+        '',
+      features: await getProductFeaturesState(
+        account_id,
+        pricingTableProduct.stripeProductId,
+      ),
+      monthly_stripe_price:
+        pricingTableProduct.monthlyStripePrice as Stripe.Price,
+      annual_stripe_price:
+        pricingTableProduct.annualStripePrice as Stripe.Price,
+    })
+  }
+  return res.status(200).json(data)
 }
 
 export default handler
