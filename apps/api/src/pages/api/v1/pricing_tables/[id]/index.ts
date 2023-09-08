23f3ab0f93013bd5688025d32a9c1d9a2493b9d3
Hunter Clarke <hunter@hunterclarke.me>
2023-09-07 18:46:54 -0600

    sorts pricing tables correctly

--- apps/api/src/pages/api/v1/pricing_tables/[id]/index.ts
+++ apps/api/src/pages/api/v1/pricing_tables/[id]/index.ts
@@ -76,6 +76,7 @@ const handler = async (
   const pricingTable = pricingTablesResult[0]
   const monthlyStripePrices = alias(stripePrices, 'monthlyStripePrices')
   const annualStripePrices = alias(stripePrices, 'annualStripePrices')
+
   const ptProducts = await db
     .select({
       id: pricingTableProducts.id,
@@ -124,27 +125,64 @@ const handler = async (
     intervals.push('year')
   }
 
-  const products = ptProducts.map((ptp) => {
-    const prices: any = {}
-    if (ptp.monthlyStripePrice) {
-      prices.month = {
-        unit_amount: (ptp.monthlyStripePrice as Stripe.Price).unit_amount,
-        currency: (ptp.monthlyStripePrice as Stripe.Price).currency,
+  const products = ptProducts
+    .map((ptp) => {
+      const prices: any = {}
+      if (ptp.monthlyStripePrice) {
+        prices.month = {
+          unit_amount: (ptp.monthlyStripePrice as Stripe.Price).unit_amount,
+          currency: (ptp.monthlyStripePrice as Stripe.Price).currency,
+        }
       }
-    }
-    if (ptp.annualStripePrice) {
-      prices.year = {
-        unit_amount: (ptp.annualStripePrice as Stripe.Price).unit_amount,
-        currency: (ptp.annualStripePrice as Stripe.Price).currency,
+      if (ptp.annualStripePrice) {
+        prices.year = {
+          unit_amount: (ptp.annualStripePrice as Stripe.Price).unit_amount,
+          currency: (ptp.annualStripePrice as Stripe.Price).currency,
+        }
       }
-    }
-    return {
-      id: ptp.stripeProductId,
-      features: states[ptp.stripeProductId],
-      name: (ptp.stripeJson as Stripe.Product).name,
-      prices,
-    }
-  })
+      return {
+        id: ptp.stripeProductId,
+        features: states[ptp.stripeProductId],
+        name: (ptp.stripeJson as Stripe.Product).name,
+        prices,
+      }
+    })
+    .sort((a, b) => {
+      if (pricingTable.monthlyEnabled) {
+        if (
+          a.prices.month &&
+          a.prices.month.unit_amount &&
+          b.prices.month &&
+          b.prices.month.unit_amount
+        ) {
+          return (
+            (a.prices.month.unit_amount as number) -
+            (b.prices.month.unit_amount as number)
+          )
+        } else if (a.prices.month) {
+          return -1
+        } else if (b.prices.month) {
+          return 1
+        }
+      } else if (pricingTable.annualEnabled) {
+        if (
+          a.prices.year &&
+          a.prices.year.unit_amount &&
+          b.prices.year &&
+          b.prices.year.unit_amount
+        ) {
+          return (
+            (a.prices.year.unit_amount as number) -
+            (b.prices.year.unit_amount as number)
+          )
+        } else if (a.prices.year) {
+          return -1
+        } else if (b.prices.year) {
+          return 1
+        }
+      }
+      return 0
+    })
 
   return res.status(200).json({
     id: pricingTable.encodedId as string,
