a4bec2eb3f79ed9e6ba637d4df0b698e7b642886
Hunter Clarke <hunter@hunterclarke.me>
2024-01-05 09:04:30 -0700

    fixes redirect

--- apps/api/src/pages/api/signup.ts
+++ apps/api/src/pages/api/signup.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body)
-  res.status(301).send('https://www.spackle.so/signed-up')
+  res.redirect(301, 'https://www.spackle.so/signed-up')
 }
 
 export default handler
