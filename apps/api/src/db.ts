f709f4bd725fab4bbf53e79b3557ce619898375d
Hunter Clarke <hunter@hunterclarke.me>
2024-01-06 11:41:19 -0700

    fixes type errors on decodePk function

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from '../drizzle/schema'
 import { sql } from 'drizzle-orm'
-import { PgBigSerial53 } from 'drizzle-orm/pg-core'
+import { PgColumn } from 'drizzle-orm/pg-core'
 
 export const conn = postgres(
   process.env.DATABASE_URL ??
@@ -12,11 +12,11 @@ const db = drizzle(conn, { schema })
 
 export const DB_PK_SALT = process.env.DB_PK_SALT ?? 'db_pk_salt'
 
-export const encodePk = (id: PgBigSerial53<any>) => {
+export const encodePk = (id: PgColumn<any>) => {
   return sql`id_encode(${id}, ${DB_PK_SALT}, 8)`
 }
 
-export const decodePk = (field: PgBigSerial53<any>, id: string) => {
+export const decodePk = (field: PgColumn<any>, id: string) => {
   return sql`${field} = (id_decode(${id}, ${DB_PK_SALT}, 8))[1]`
 }
 
