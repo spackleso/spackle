aa59b9225e41b3c19c5c0642f654abcea20a487d
Hunter Clarke <hunter@hunterclarke.me>
2024-02-26 09:07:42 -0700

    adds back bcc

--- apps/api/src/pages/api/signup.ts
+++ apps/api/src/pages/api/signup.ts
@@ -57,6 +57,7 @@ const sendSignupEmail = async (email: string) => {
   await client.sendEmail({
     From: 'hunter@spackle.so',
     To: email,
+    Bcc: 'hunter@spackle.so',
     Subject: 'Welcome to Spackle!',
     TextBody: emailText,
     HtmlBody: emailHtml,
