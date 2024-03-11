55ef4e07a52817f2a28e9783391c3010d69df47d
Hunter Clarke <hunter@hunterclarke.me>
2024-03-11 15:13:07 -0600

    implements an index route handler for the api

--- apps/api-next/src/routes/v1/index.ts
+++ apps/api-next/src/routes/v1/index.ts
@@ -1,8 +1,12 @@
-import { Hono } from 'hono'
+import { Hono, Context } from 'hono'
 import customers from './customers'
 
 const app = new Hono()
 
 app.route('/customers', customers)
+
+app.get('/', (c: Context) => {
+  return c.json({ message: 'Spackle API V1' })
+})
 
 export default app
