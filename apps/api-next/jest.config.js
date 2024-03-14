d5c9deb038470606884a558d5bd6ca2bc85d8e4d
Hunter Clarke <hunter@hunterclarke.me>
2024-03-14 09:27:54 -0600

    removes built jest config file

--- apps/api-next/jest.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-};
-export default config;
